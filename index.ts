f93df7b046fe820514bc4ddd43965c056153ea59
Augusto Silva <augusto.a.silva@hotmail.com>
2021-02-20 17:49:05 -0300

    Add the body-parser and init the index route

--- index.ts
+++ index.ts
@@ -1,7 +1,16 @@
-import express from 'express';
-const app = express();
-const PORT = 3000;
-app.get('/', (req, res) => res.send('Express + TypeScript Server is awesome!'));
+import express from 'express'
+import bodyParser from 'body-parser'
+import indexRoutes from './routes/index'
+
+const app = express()
+const PORT: number = 3000
+
+// Aplicando o parser de JSON em todas as rotas.
+app.use(bodyParser.json())
+
+// Inicializando o arquivo de rotas da 'home'
+app.use(indexRoutes)
+
 app.listen(PORT, () => {
-    console.log(`[server]: Server is running at http://localhost:${PORT}`);
-});
+  console.log(`Servidor rodando em http://localhost:${PORT}`)
+})
