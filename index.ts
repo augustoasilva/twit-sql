bd1f5f8e10af7a73f60687b469dcedee05d18dc1
Augusto Silva <augusto.a.silva@hotmail.com>
2021-03-01 19:09:25 -0300

    Adds cors and helmet packages

--- index.ts
+++ index.ts
@@ -1,9 +1,17 @@
 import express, { Application } from 'express'
 import bodyParser from 'body-parser'
+import cors from 'cors'
+import helment from 'helmet'
 import * as routes from './routes/index'
 
 const app: Application = express()
 const PORT: number = 3000
+
+// Aplicando o CORS em todas as rotas.
+app.use(cors())
+
+// Aplicando o HELMET em todas as rotas.
+app.use(helment())
 
 // Aplicando o parser de JSON em todas as rotas.
 app.use(bodyParser.json())
